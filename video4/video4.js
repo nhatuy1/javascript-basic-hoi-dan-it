c237c1cab7c8ae8b35a5c8f51d261b6d04f18324
nhat huy <trinhnhathuycm@gmail.com>
2022-06-16 15:26:37 +0400

    chapter2 video6

--- video4/video4.js
+++ video4/video4.js
@@ -1,2 +1,4 @@
-let a = 'tôi là một string-chuỗi'
-console.log(a)
+let a = 'tôi là một string-chuỗi';
+let text = `We are the so-called "Vikings" from the north.`;
+console.log(text);
+console.log(a);
