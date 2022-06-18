ee6cf7b5f00351dd0ba3ade10a28db4eb503abef
nhat huy <trinhnhathuycm@gmail.com>
2022-06-18 14:17:30 +0400

    chapter2 video9

--- video8/video8.js
+++ video8/video8.js
@@ -8,6 +8,6 @@ let obj = {
 let b = 'name';
 obj[b] = 'harry';// truyền giá trị harry vào key name
 obj.b = 'ali';// truyền key b: value ali vào obj
-obj.name = 'amanda';
+obj.name = 'amanda';// truyền chuỗi giá trị amanda vào key name
 console.log('what is your name?', `my name's : `, obj['name'], obj);
 console.log('where are you from?', `I'm from : `, obj.adrress);
---
+++ video9/video9.js
@@ -0,0 +1,17 @@
+console.log('Hello World from HTML');
+let a={
+    name: 'HUY',
+    age: 25,
+
+};
+console.log( 'tên của tôi là:', a.name,'tuổi cua tôi là:',a.age);
+console.log('check data object:',a)
+// arr = [item1, item2,item3, ....]
+let b = ['MU','Chelsea','Liver'];
+console.log('type of b:', typeof b,b)
+console.log('first item:', b[0],'second item:', b[1])
+let family = [ 
+    {name: 'HUY',age: 25},
+    {name: 'NHI',age: 17}
+]
+console.log('my family: ',family)
