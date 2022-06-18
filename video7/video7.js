b9942bba9de93d44169230b7508029791a8a7bb3
nhat huy <trinhnhathuycm@gmail.com>
2022-06-18 13:16:37 +0400

    chapter2 video7

---
+++ video7/video7.js
@@ -0,0 +1,7 @@
+console.log('Hello World from HTML');
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.');
+console.log(`Hỏi dân it check strings: a = ${a} b = ${b} c = ${a+b}`);
+console.log(`/hoidanit/${a}`)
