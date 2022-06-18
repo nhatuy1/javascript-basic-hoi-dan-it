37a9b15588bcd3db5cb275cf87b96b660c2dd758
nhat huy <trinhnhathuycm@gmail.com>
2022-06-18 14:54:02 +0400

    chapter2 video10

---
+++ video10/video10.js
@@ -0,0 +1,17 @@
+console.log('XIN CHÀO MỌI NGƯỜI');
+let a = 5;
+let b = '5';
+console.log(' compare a == b:','a=', a, 'b =', b, 'result:',a==b)
+console.log(' compare a === b:','a=', a, 'b =', b, 'result:',a===b)
+//===: type, value
+//==: value
+
+//empty,null,undefined
+let c= [];
+console.log('check value c:', c)
+let d
+d=5;
+d=['hello']
+console.log('check value d:', d)
+
+
