511647a4f972d14708c589564457b59b0ee106c3
nhat huy <trinhnhathuycm@gmail.com>
2022-06-18 13:53:00 +0400

    chapter2 video8

---
+++ video8/video8.js
@@ -0,0 +1,13 @@
+console.log('Hello World from HTML');
+
+//key:value
+let obj = {
+    name: 'Huy',
+    adrress: 'Cà Mau',
+};
+let b = 'name';
+obj[b] = 'harry';// truyền giá trị harry vào key name
+obj.b = 'ali';// truyền key b: value ali vào obj
+obj.name = 'amanda';
+console.log('what is your name?', `my name's : `, obj['name'], obj);
+console.log('where are you from?', `I'm from : `, obj.adrress);
