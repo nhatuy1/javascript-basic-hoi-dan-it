135948d0b7701bcc0b2b3ebaf8fad661978a1de2
nhat huy <trinhnhathuycm@gmail.com>
2022-06-19 16:45:24 +0400

    chapter3 video13

---
+++ video13/video13.js
@@ -0,0 +1,14 @@
+console.log('hello world from html')
+
+let arrTop4=['live','chelsea','Mu','Mancity']
+let i=0;
+while(i< arrTop4.length){
+    i++
+     if(arrTop4[i]==='chelsea')
+     {
+        console.log('found it:', arrTop4[i])
+        continue;
+     }
+     console.log('kiểm tra i =', i)
+  
+    }
