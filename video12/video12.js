6689a9157b4b661f8b6923a0f9d608c193ed5fa3
nhat huy <trinhnhathuycm@gmail.com>
2022-06-19 16:05:53 +0400

    chapter3 video12

---
+++ video12/video12.js
@@ -0,0 +1,18 @@
+console.log('Hello world from HTML');
+let arrTop4= [' man city','livepool','Mu','chelsea'];
+
+// for (let i = 0; i < arrTop4.length; i++) {
+//     console.log('top ', i+1, ':',arrTop4)
+    
+// }
+let i=0
+// while(i<arrTop4.length){
+// console.log('top ',i+1,arrTop4[i]);
+// i++;
+// }
+let flag= false;
+while(!flag){
+console.log('top ', i+1,arrTop4[i]);
+i++;
+if(i===4) flag=true
+}
